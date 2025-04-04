5fc7f0e5b28e25cd2babc69e6b2c563c9777bde0
Justin Rocan <justicus.dev@outlook.com>
2025-04-04 17:46:30 -0500

    Cleaned up CSS and classes

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const timelineCards = document.querySelectorAll('.timeline__component--bg');
-    
-    timelineCards.forEach(card => {
-        card.addEventListener('click', function() {
-            this.classList.toggle('expanded');
-        });
-    });
-});
